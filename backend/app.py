3f696a1db6fb4530680c5382ec6923086f3e5067
vantage-ola <vantageola4@gmail.com>
2023-12-04 21:42:47 +0100

    connect-db, test 'cars-collection'

--- backend/app.py
+++ backend/app.py
@@ -1,20 +1,26 @@
-from flask import Flask, jsonify
+from flask import Flask, request, Response
+from pymongo import MongoClient
+from decouple import config
+from bson.json_util import dumps
+
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return 'Hello, World'
+#mongo db config
+MONGO_USERNAME = config('MONGO_USERNAME')
+MONGO_PASSWORD = config('MONGO_PASSWORD')
+
+client =MongoClient("mongodb+srv://{}:{}@cluster0.j1gr1sc.mongodb.net/?retryWrites=true&w=majority".format(MONGO_USERNAME,MONGO_PASSWORD))
+db = client['track_now'] #Track Now DB
+
+
+
 
 @app.route('/api/cars')
 def get_cars():
-    cars =[{'id': 1, 'name': 'Mercedes-AMG One'}]
-    return jsonify(cars)
+    cars = list(db.cars.find())
+    response = Response(response=dumps(cars), status=200, mimetype="application/json")
 
-@app.route('/api/tracks')
-def get_tracks():
-    tracks =[{'id': 1, 'name': 'Nürburgring Nordschleife', 'distance' : '12.8mi'}]
-    return jsonify(tracks)
-
+    return response
 if __name__ == '__main__':
     app.run(debug = True)
