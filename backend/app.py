7ff4773e22719fd0f31e021d09bd3c12b4b102ed
vantage-ola <vantageola4@gmail.com>
2023-12-09 23:49:33 +0100

    add-cars+tracks-routes

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,8 @@
-from flask import Flask, request, Response
+from flask import Flask, request,jsonify
 from pymongo import MongoClient
 from decouple import config
-from bson.json_util import dumps
+
+from models import Car, Track
 
 
 app = Flask(__name__)
@@ -16,11 +17,74 @@ db = client['track_now'] #Track Now DB
 
 
 
-@app.route('/api/cars')
-def get_cars():
-    cars = list(db.cars.find())
-    response = Response(response=dumps(cars), status=200, mimetype="application/json")
+@app.route('/api/cars', methods=['GET', 'POST'])
+def handle_cars():
 
-    return response
+    if request.method == 'GET':
+        cars =  db.cars.find({}, {'_id': False})
+        car_objects = [Car(**car) for car in cars]
+        car_dicts = [car.to_dict() for car in car_objects]
+        return jsonify(car_dicts)
+    
+    elif request.method == 'POST':
+        data = request.json
+
+        #if inserting multiple data
+        if isinstance(data, list):
+            # Insert multiple cars
+            new_cars = [Car(**car) for car in data]
+            result = db.cars.insert_many([car.to_dict() for car in new_cars])
+            return jsonify({'message': f'{len(result.inserted_ids)} Cars added successfully!'})
+
+        #only one data
+        else:
+
+            new_car = Car(
+                id=data['id'],
+                name=data['name'],
+                body=data['body'],
+                car_class=data['car_class'],
+                engine=data['engine'],
+                hp=data['hp'],
+                layout=data['layout'],
+                racecar=data['racecar'],
+                transmission=data['transmission']
+            )
+            db.cars.insert_one(new_car.to_dict())
+            return jsonify({'message': 'Car added successfully!'})
+
+
+@app.route('/api/tracks', methods=['GET', 'POST'])
+def handle_tracks():
+
+    if request.method == 'GET':
+        tracks = db.tracks.find({}, {'_id': False})
+        track_objects = [Track(**track) for track in tracks]
+        track_dicts = [track.to_dict() for track in track_objects]
+        return jsonify(track_dicts)
+    
+    elif request.method == 'POST':
+        
+        data = request.json
+        
+        if isinstance(data, list):
+            new_tracks = [Track(**track) for track in data]
+            result = db.tracks.insert_many([track.to_dict() for track in new_tracks])
+            
+            return jsonify({'message': f'{len(result.inserted_ids)} Tracks added successfully!'})
+
+        else:            
+            new_track = Track(
+                id=data['id'],
+                name=data['name'],
+                location=data['location'],
+                grade=data['grade'],
+                length=data['length']
+            )
+            db.tracks.insert_one(new_track.to_dict())
+            return jsonify({'message': 'Track added successfully!'})
+
+
+
 if __name__ == '__main__':
     app.run(debug = True)
