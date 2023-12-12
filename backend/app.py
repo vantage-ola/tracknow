5c4cc62ad0a39ff4cedf6c7987eeb881a166add8
vantage-ola <vantageola4@gmail.com>
2023-12-12 15:12:15 +0100

    add-drivers+laptimes-routes

--- backend/app.py
+++ backend/app.py
@@ -2,7 +2,7 @@ from flask import Flask, request,jsonify
 from pymongo import MongoClient
 from decouple import config
 
-from models import Car, Track
+from models import Car, Track, Laptime ,Driver
 
 
 app = Flask(__name__)
@@ -84,6 +84,62 @@ def handle_tracks():
             db.tracks.insert_one(new_track.to_dict())
             return jsonify({'message': 'Track added successfully!'})
 
+
+
+@app.route('/api/drivers', methods=['GET', 'POST'])
+def handle_drivers():
+    if request.method == 'GET':
+        drivers = db.drivers.find({}, {'_id': False})
+        driver_objects = [Driver(**driver) for driver in drivers]
+        driver_dicts = [driver.to_dict() for driver in driver_objects]
+        return jsonify(driver_dicts)
+    
+    elif request.method == 'POST':
+        data = request.json
+        
+        if isinstance(data, list):
+            new_drivers = [Driver(**driver) for driver in data]
+            result = db.drivers.insert_many([driver.to_dict() for driver in new_drivers])
+            
+            return jsonify({'message': f'{len(result.inserted_ids)} Drivers added successfully!'})
+
+        else:            
+            new_driver = Driver(
+                id=data['id'],
+                name=data['name'],
+                nationality=data['nationality']
+            )
+            db.drivers.insert_one(new_driver.to_dict())
+            return jsonify({'message': 'Driver added successfully!'})
+
+
+@app.route('/api/laptimes', methods=['GET', 'POST'])
+def handle_laptimes():
+    if request.method == 'GET':
+        laptimes = db.laptimes.find({}, {'_id': False})
+        laptime_objects = [Laptime(**laptime) for laptime in laptimes]
+        laptime_dicts = [laptime.to_dict() for laptime in laptime_objects]
+        return jsonify(laptime_dicts)
+    
+    elif request.method == 'POST':
+        data = request.json
+        
+        if isinstance(data, list):
+            new_laptimes = [Laptime(**laptime) for laptime in data]
+            result = db.laptimes.insert_many([laptime.to_dict() for laptime in new_laptimes])
+            
+            return jsonify({'message': f'{len(result.inserted_ids)} Laptimes added successfully!'})
+
+        else:            
+            new_laptime = Laptime(
+                car_id=data['car_id'],
+                track_id=data['track_id'],
+                driver_id=data['driver_id'],
+                time=data['time'],
+                date=data['date']
+            )
+            db.laptimes.insert_one(new_laptime.to_dict())
+            return jsonify({'message': 'Laptime added successfully!'})
 
 
 if __name__ == '__main__':
