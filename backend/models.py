c68a78969e5e795624ff1bb98b7211f37963ed90
Dozie2001 <davidchidozie402@gmail.com>
2023-12-20 14:33:55 +0100

    Fixed README and added track_id

--- backend/models.py
+++ backend/models.py
@@ -39,42 +39,6 @@ class Driver:
             'nationality': self.nationality
         }
 
-# Laptime Model
-class Laptime(Car, Driver):
-    
-    # Inherits the Car id from Car and the driver Id from Driver
-    def __init__(self, car_id, track_id, driver_id, time, date):
-        # The contructor files form Car and Driver
-        Car.__init__(self, id=car_id, name="", body="", car_class="", engine="", hp=0, layout="", racecar=False, transmission="")
-        Driver.__init__(self, id=driver_id,  name="", nationality="")
-
-        # Remaining attributes
-        self.track_id = track_id
-        self.time = time
-        self.date = date
-
-    def to_dict(self):
-        dict_car = Car.to_dict(self)
-        dict_Driver = Driver.to_dict(self)
-        return {
-            'car_id': dict_car['id'],
-            'track_id': self.track_id,
-            'driver_id':  dict_Driver['id'],
-            'time': self.time,
-            'date' : self.date
-        }
-    def only_tracks(self):
-        return {
-            'track_id': self.track_id
-        }
-    def only_drivers(self):
-        return {
-            'driver_id': self.Car.id
-        }
-    def only_cars(self):
-        return {
-            'car_id' : self.Driver.id
-        }
 # Track Model
 class Track:
     def __init__(self, id, name, location, grade, length):
@@ -92,3 +56,41 @@ class Track:
             'grade': self.grade,
             'length': self.length
         }
+
+# Laptime Model
+class Laptime(Car, Driver, Track):
+    
+    # Inherits the Car id from Car and the driver Id from Driver
+    def __init__(self, car_id, track_id, driver_id, time, date):
+        # The contructor files form Car and Driver
+        Car.__init__(self, id=car_id, name="", body="", car_class="", engine="", hp=0, layout="", racecar=False, transmission="")
+        Driver.__init__(self, id=driver_id,  name="", nationality="")
+        Track.__init__(self, id=track_id, name="", location={}, grade="", length=0)
+
+        #Remaining attributes
+        self.time = time
+        self.date = date
+
+    def to_dict(self):
+        dict_car = Car.to_dict(self)
+        dict_Driver = Driver.to_dict(self)
+        dict_Track = Track.to_dict(self)
+        return {
+            'car_id': dict_car['id'],
+            'track_id': dict_Track['id'],
+            'driver_id':  dict_Driver['id'],
+            'time': self.time,
+            'date' : self.date
+        }
+    def only_tracks(self):
+        return {
+            'track_id': self.track_id
+        }
+    def only_drivers(self):
+        return {
+            'driver_id': self.Car.id
+    }
+    def only_cars(self):
+        return {
+            'car_id' : self.Driver.id
+    }
