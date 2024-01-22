cafbd8f09c0226e6a7d70ef93483d8e7f73cb5bd
Oluwasanya Olaoluwa <vantageola4@gmail.com>
2024-01-22 14:21:16 +0100

    restructure/ +bottom-tab-navigator

---
+++ frontend/tracknow/components/Driver/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Driver = () => {
+  return (
+    <View>
+      <Text>Driver View</Text>
+    </View>
+  );
+};
+
+export default Driver;
