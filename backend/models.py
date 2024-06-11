f3e39ad081ae136d0fae4a161cbb4b3ba16ba259
vantage-ola <vantageola4@gmail.com>
2024-06-11 10:07:00 +0100

    change to password-hash coz of security

--- backend/models.py
+++ backend/models.py
@@ -9,7 +9,7 @@ class User(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    password = db.Column(db.String(120), nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
     laptimes = db.relationship('Laptime', backref='user', lazy=True)
 
     def __repr__(self):
