0b3bad6d1b51df5daf6d23b35a0cb84cb996db9d
vantage-ola <vantageola4@gmail.com>
2024-06-11 11:27:07 +0100

    init-test

---
+++ backend/tests/test_user.py
@@ -0,0 +1,13 @@
+from flask import json
+
+def test_login_user():
+    # TODO test user logging in. 
+    pass
+
+def test_create_user():
+    # TODO test cases: create user, existing user,missing parameters.
+    pass
+
+def test_user_jwt():
+    # TODO test logged in user access tokens.
+    pass
