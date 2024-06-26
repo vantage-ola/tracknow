2693f8c1b8d1849d53f9293be2a4b39383040110
vantage-ola <vantageola4@gmail.com>
2024-06-26 15:01:03 +0100

    made design better on login and sign up page

--- tracknow/web/src/components/User/UserLogin.tsx
+++ tracknow/web/src/components/User/UserLogin.tsx
@@ -1,42 +1,46 @@
 import * as React from "react";
 import { Flex, Input, Button, Image, Text, Link } from "@chakra-ui/react";
 import { Link as ReactRouterLink } from 'react-router-dom';
+import { Navbar } from "../Navbar/Navbar";
 
 export const UserLogin = () => {
 
     return (
-        <Flex h="100vh" alignItems="center" justifyContent="center">
-            <Flex
-                flexDirection="column"
-                p={12}
-                borderRadius={8}
-                boxShadow="lg"
-            >
-                <Image boxSize="80px" alignSelf="center" src='./web_svg/arrow_done.svg' alt='track-bnow img' />
+        <>
+            <Navbar />
+            <Flex h="80vh" alignItems="center" justifyContent="center">
+                <Flex
+                    flexDirection="column"
+                    p={12}
+                    borderRadius={8}
+                    boxShadow="lg"
+                >
+                    <Image boxSize="80px" alignSelf="center" src='./web_svg/arrow_done.svg' alt='track-bnow img' />
 
-                <Text marginBottom="10px" alignSelf="center" fontSize='xs'>Welcome, driver. Sign in to continue</Text>
+                    <Text marginBottom="10px" alignSelf="center" fontSize='xs'>Welcome, driver. Sign in to continue</Text>
 
-                <Input
-                    focusBorderColor='red.500'
-                    placeholder="Username"
-                    type="text"
-                    variant="outline"
-                    mb={3}
-                />
-                <Input
-                    focusBorderColor='red.500'
-                    placeholder="Password"
-                    type="password"
-                    variant="outline"
-                    mb={3}
-                />
-                <Button colorScheme="red" mb={3}>
-                    Sign In
-                </Button>
-                <Text alignSelf="center" fontSize='xs'>New here? <Link as={ReactRouterLink} to='/create-user' color="red"> Create an account</Link></Text>
+                    <Input
+                        focusBorderColor='red.500'
+                        placeholder="Username"
+                        type="text"
+                        variant="outline"
+                        mb={3}
+                    />
+                    <Input
+                        focusBorderColor='red.500'
+                        placeholder="Password"
+                        type="password"
+                        variant="outline"
+                        mb={3}
+                    />
+                    <Button colorScheme="red" mb={3}>
+                        Sign In
+                    </Button>
+                    <Text alignSelf="center" fontSize='xs'>New here? <Link as={ReactRouterLink} to='/create-user' color="red"> Create an account</Link></Text>
+                </Flex>
             </Flex>
-        </Flex>
+        </>
     )
 };
 
-// TODO move new? here... closer to the sign in button and change link colour to red. 
+// TODO change link colour to red. 
--- tracknow/web/src/components/User/UserSignUp.tsx
+++ tracknow/web/src/components/User/UserSignUp.tsx
@@ -1,38 +1,42 @@
 import * as React from "react";
 import { Flex, Input, Button, Image, Text, Link } from "@chakra-ui/react";
 import { Link as ReactRouterLink } from 'react-router-dom';
+import { Navbar } from "../Navbar/Navbar";
 
 export const UserSignUp = () => {
 
     return (
-        <Flex h="100vh" alignItems="center" justifyContent="center">
-            <Flex
-                flexDirection="column"
-                p={12}
-                borderRadius={8}
-                boxShadow="lg"
-            >
-                <Image boxSize="80px" alignSelf="center" src='./web_svg/arrow_done.svg' alt='track-bnow img' />
-                <Text marginBottom="10px" alignSelf="center" fontSize='xs'>Welcome, driver. Create an account</Text>
-                <Input
-                    focusBorderColor='red.500'
-                    placeholder="Username"
-                    type="text"
-                    variant="outline"
-                    mb={3}
-                />
-                <Input
-                    focusBorderColor='red.500'
-                    placeholder="Password"
-                    type="password"
-                    variant="outline"
-                    mb={3}
-                />
-                <Button colorScheme="red" mb={3}>
-                    Create an Account
-                </Button>
-                <Text alignSelf="center" fontSize='xs'>You have an account?<Link as={ReactRouterLink} to='/login' color="red"> Sign in here</Link></Text>
+        <>
+            <Navbar />
+            <Flex h="80vh" alignItems="center" justifyContent="center">
+                <Flex
+                    flexDirection="column"
+                    p={12}
+                    borderRadius={8}
+                    boxShadow="lg"
+                >
+                    <Image boxSize="80px" alignSelf="center" src='./web_svg/arrow_done.svg' alt='track-bnow img' />
+                    <Text marginBottom="10px" alignSelf="center" fontSize='xs'>Welcome, driver. Create an account</Text>
+                    <Input
+                        focusBorderColor='red.500'
+                        placeholder="Username"
+                        type="text"
+                        variant="outline"
+                        mb={3}
+                    />
+                    <Input
+                        focusBorderColor='red.500'
+                        placeholder="Password"
+                        type="password"
+                        variant="outline"
+                        mb={3}
+                    />
+                    <Button colorScheme="red" mb={3}>
+                        Create an Account
+                    </Button>
+                    <Text alignSelf="center" fontSize='xs'>You have an account?<Link as={ReactRouterLink} to='/login' color="red"> Sign in here</Link></Text>
+                </Flex>
             </Flex>
-        </Flex>
+        </>
     )
 };
