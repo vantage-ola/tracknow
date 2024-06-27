1835ed876ed7d3f5fdc2adcbb1f53956f7c9bc0d
vantage-ola <vantageola4@gmail.com>
2024-06-27 16:17:37 +0100

    +carousel component, react slick

---
+++ tracknow/web/src/components/Home/CarouselCard.tsx
@@ -0,0 +1,117 @@
+import * as React from "react";
+import {
+    Box,
+    Heading,
+    Text,
+    Flex,
+    Center,
+    useColorModeValue,
+    HStack,
+    AspectRatio,
+    Icon
+} from "@chakra-ui/react";
+import { BsArrowUpRight, BsHeartFill, BsHeart } from "react-icons/bs";
+import { FaClock, FaLaptop, FaMapMarkedAlt } from "react-icons/fa";
+import { Laptime, SignUpResponse } from "../../Types";
+import Marquee from "../Marquee/Marquee";
+
+
+
+const CarouselCard: React.FC<{ laptime: Laptime, username: SignUpResponse }> = ({ laptime, username }) => {
+    const [liked, setLiked] = React.useState(false);
+    const { car, track, time, platform, comment, youtube_link } = laptime;
+    const videoId = youtube_link?.split("v=")[1];
+
+    return (
+        <Center py={3} mt={{ base: 90, md: 0 }}>
+            <Box
+                w="xs"
+                rounded={"sm"}
+                my={5}
+                mx="auto"
+                overflow={"hidden"}
+                bg="white"
+                border={"1px"}
+                borderColor="black"
+                boxShadow={useColorModeValue("6px 6px 0 red", "6px 6px 0 cyan")}
+
+            >
+                {youtube_link && (
+                    <AspectRatio ratio={16 / 9}>
+                        <iframe
+                            title="YouTube video player"
+                            src={`https://www.youtube.com/embed/${videoId}`}
+                            allowFullScreen
+                        />
+                    </AspectRatio>
+                )}
+                <Box p={4}  >
+                    <Box
+                        bg="black"
+                        display={"inline-block"}
+                        px={2}
+                        py={1}
+                        color="white"
+                        mb={2}
+                    >
+                        <Text fontSize={"xs"} fontWeight="medium">
+                            {car}
+                        </Text>
+                    </Box>
+                    <Heading color={"black"} fontSize={"2xl"} whiteSpace="nowrap">
+                        <Marquee text={comment} />
+                    </Heading>
+
+                    <Flex align="center">
+                        <Icon color={"black"} as={FaMapMarkedAlt} mr={2} />
+                        <Text color={"black"}>{track}</Text>
+                    </Flex>
+
+                    <Flex align="center">
+                        <Icon color={"black"} as={FaClock} mr={2} />
+                        <Text color="black">{time}</Text>
+                    </Flex>
+
+                    {platform && (
+                        <Flex align="center">
+                            <Icon color="black" as={FaLaptop} mr={2} />
+                            <Text color="black">{platform}</Text>
+                        </Flex>
+                    )}
+                </Box>
+                <HStack borderTop={"1px"} color="black">
+                    <Flex
+                        p={4}
+                        alignItems="center"
+                        justifyContent={"space-between"}
+                        roundedBottom={"sm"}
+                        cursor={"pointer"}
+                        w="full"
+                    >
+                        <Text fontSize={"md"} fontWeight={"semibold"}>
+                            @{username.username}
+                        </Text>
+                        <BsArrowUpRight />
+                    </Flex>
+                    <Flex
+                        p={4}
+                        alignItems="center"
+                        justifyContent={"space-between"}
+                        roundedBottom={"sm"}
+                        borderLeft={"1px"}
+                        cursor="pointer"
+                        onClick={() => setLiked(!liked)}
+                    >
+                        {liked ? (
+                            <BsHeartFill fill="red" fontSize={"24px"} />
+                        ) : (
+                            <BsHeart fontSize={"24px"} />
+                        )}
+                    </Flex>
+                </HStack>
+            </Box>
+        </Center>
+    );
+};
+
+export default CarouselCard;
