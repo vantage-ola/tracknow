008c719744a0ce90373899faec5560551d1f2a5f
vantage-ola <vantageola4@gmail.com>
2024-06-27 16:18:37 +0100

    navbar logged in + marquee components(carousel card)

--- tracknow/web/src/components/Navbar/Navbar.tsx
+++ tracknow/web/src/components/Navbar/Navbar.tsx
@@ -3,18 +3,31 @@ import {
     Box,
     Flex,
     Text,
-    Link,
     Stack,
-    Center,
     Button,
-    useColorModeValue
-} from '@chakra-ui/react';
+    Center,
+    useColorModeValue,
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton,
+    useDisclosure,
+    FormControl,
+    FormLabel,
+    Input,
+    Checkbox,
+    Select,
+    Link
+} from "@chakra-ui/react";
 import { PlusSquareIcon } from '@chakra-ui/icons'
 import { Link as ReactRouterLink } from 'react-router-dom';
 import { SignUpResponse } from "../../Types";
 
 export const NavbarWelcome = () => (
-    <Box px={4} borderBottom={2} borderStyle={'solid'} borderColor={useColorModeValue('red', 'white')}>
+    <Box px={4} borderBottom={1} borderStyle={'solid'} borderColor={useColorModeValue('red', 'white')}>
         <Flex h={10} alignItems={'center'} justifyContent={'space-between'}>
 
             <Box><Text fontSize="xl" as="b">tracknow</Text></Box>
@@ -32,7 +45,7 @@ export const NavbarWelcome = () => (
 );
 
 export const Navbar = () => (
-    <Box px={4} borderBottom={2} borderStyle={'solid'} borderColor={useColorModeValue('red', 'white')}>
+    <Box px={4} borderBottom={1} borderStyle={'solid'} borderColor={useColorModeValue('red', 'white')}>
         <Flex h={10} alignItems={'center'} justifyContent={'space-between'}>
 
             <Box><Text fontSize="xl" as="b">tracknow</Text></Box>
@@ -49,31 +62,100 @@ export const Navbar = () => (
 );
 
 export const NavbarLoggedIn = ({ username }: SignUpResponse) => {
+    const { isOpen, onOpen, onClose } = useDisclosure();
 
+    const [car, setCar] = React.useState("");
+    const [track, setTrack] = React.useState("");
+    const [time, setTime] = React.useState("");
+    const [youtubeLink, setYoutubeLink] = React.useState("");
+    const [simracing, setSimracing] = React.useState(false);
+    const [platform, setPlatform] = React.useState("");
+
+    const handleSubmit = () => {
+        // Add code to handle form submission here
+        onClose();
+    };
     return (
-        <Box px={4} borderBottom={2} borderStyle={'solid'} borderColor={useColorModeValue('red', 'white')}>
-            <Flex h={10} alignItems={'center'} justifyContent={'space-between'}>
+        <Box
+            px={4}
+            borderBottom={1}
+            borderStyle={"solid"}
+            borderColor={useColorModeValue("red", "white")}
+        >
+            <Flex h={10} alignItems={"center"} justifyContent={"space-between"}>
+                <Box>
+                    <Text fontSize="xl" as="b">
+                        tracknow
+                    </Text>
+                </Box>
 
-                <Box><Text fontSize="xl" as="b">tracknow</Text></Box>
-
-                <Flex alignItems={'center'}>
-                    <Stack direction={'row'} spacing={1}>
-
+                <Flex alignItems={"center"}>
+                    <Stack direction={"row"} spacing={1}>
                         <Button
                             top={0.5}
-                            variant={'solid'}
-                            colorScheme={'red'}
-                            size={'xs'}
-                            mr={1}><PlusSquareIcon color={"black"} />
+                            variant={"solid"}
+                            colorScheme={"red"}
+                            size={"xs"}
+                            mr={1}
+                            onClick={onOpen}
+                        >
+                            <PlusSquareIcon color={"black"} />
                         </Button>
 
                         <Center>
-                            <Text  >@{username}</Text>
+                            <Text>@{username}</Text>
                         </Center>
-
                     </Stack>
                 </Flex>
             </Flex>
+
+            {/*add laptime modal*/}
+            <Modal isOpen={isOpen} onClose={onClose} motionPreset="slideInTop" size={'xs'}>
+                <ModalOverlay bg='none'
+                    backdropFilter='auto'
+                    backdropBlur='2px' />
+                <ModalContent >
+                    <ModalHeader color={"black"}>Add Laptime</ModalHeader>
+                    <ModalCloseButton color={"black"} />
+                    <ModalBody color={"black"}>
+                        <FormControl>
+                            <FormLabel>Car</FormLabel>
+                            <Input focusBorderColor='red.500' value={car} onChange={(e) => setCar(e.target.value)} />
+                        </FormControl>
+                        <FormControl>
+                            <FormLabel>Track</FormLabel>
+                            <Input focusBorderColor='red.500' value={track} onChange={(e) => setTrack(e.target.value)} />
+                        </FormControl>
+                        <FormControl>
+                            <FormLabel>Laptime</FormLabel>
+                            <Input focusBorderColor='red.500' value={time} onChange={(e) => setTime(e.target.value)} />
+                        </FormControl>
+                        <FormControl>
+                            <FormLabel>YouTube Link</FormLabel>
+                            <Input focusBorderColor='red.500' value={youtubeLink} onChange={(e) => setYoutubeLink(e.target.value)} />
+                        </FormControl>
+                        <FormControl display="flex" alignItems="center">
+                            <FormLabel mb="0">Simracing</FormLabel>
+                            <Checkbox color={'red'} isChecked={simracing} onChange={(e) => setSimracing(e.target.checked)} />
+                        </FormControl>
+                        <FormControl>
+                            <Select value={platform} onChange={(e) => setPlatform(e.target.value)} isDisabled={!simracing}>
+                                <option value="">Select platform</option>
+                                <option value="Assetto Corsa Competizione">Assetto Corsa Competizione</option>
+                                <option value="iRacing">iRacing</option>
+                                <option value="Gran Turismo 7">Gran Turismo 7</option>
+                                {/* Add more options as needed */}
+                            </Select>
+                        </FormControl>
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button color='red.500' variant="ghost" onClick={handleSubmit}>
+                            Save
+                        </Button>
+                    </ModalFooter>
+                </ModalContent>
+            </Modal>
+
         </Box>
-    )
-}
+    );
+};
