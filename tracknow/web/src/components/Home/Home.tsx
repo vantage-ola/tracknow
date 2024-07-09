a944a730baf0af0f326cb06504703ed11ceb01c8
vantage-ola <vantageola4@gmail.com>
2024-07-09 15:01:16 +0100

    home adjustments, post page. ✅

--- tracknow/web/src/components/Home/Home.tsx
+++ tracknow/web/src/components/Home/Home.tsx
@@ -1,9 +1,10 @@
 import * as React from "react";
 import { NavbarLoggedIn } from "../Navbar/Navbar";
 // import Footer from "../Footer/Footer";
-import CarouselCard from "./CarouselCard";
+// import CarouselCard from "../CarouselCard/CarouselCard";
+import { HomePost } from "../Post/Post";
 import Slider from "react-slick";
-import { Text, useToast } from "@chakra-ui/react";
+import { useToast } from "@chakra-ui/react";
 import { useNavigate } from 'react-router-dom';
 
 import API from "../../hooks/API";
@@ -11,7 +12,7 @@ import { LoadingSpinner } from "../Loading/LoadingSpinner";
 import { useLaptimes } from "../../hooks/useLaptimes";
 
 // setting for slide carousel
-const settings = {
+/*const settings = {
     dots: true,
     arrows: false,
     fade: true,
@@ -21,7 +22,7 @@ const settings = {
     slidesToShow: 1,
     slidesToScroll: 1,
 }
-
+*/
 
 export const Home = () => {
     const [slider, setSlider] = React.useState<Slider | null>(null)
@@ -59,16 +60,17 @@ export const Home = () => {
 
     if (loading) {
         return <LoadingSpinner />;
-    }
+    };
 
     return (
         <>
-            {/* CSS files for react-slick */}
+            {/* CSS files for react-slick 
             <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
             <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
-
+            */}
 
             <NavbarLoggedIn username={username} />
+            {/* carousel card...
             <Slider {...settings} ref={(slider) => setSlider(slider)}>
                 {laptime &&
                     laptime.map((laptime, index) => (
@@ -79,6 +81,9 @@ export const Home = () => {
             <Text textAlign="center" fontSize='xs' color="grey" mb={1}>
                 Swipe right or left to change
             </Text>
+            */}
+            <HomePost laptimes={laptime} />
+
         </>
     )
 };
