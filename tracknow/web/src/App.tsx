f58914647870d38058c0161a87277607cd366f78
vantage-ola <vantageola4@gmail.com>
2024-07-09 15:00:03 +0100

    move theme to its own file + new fonts + button variant.

--- tracknow/web/src/App.tsx
+++ tracknow/web/src/App.tsx
@@ -1,29 +1,12 @@
 import * as React from "react";
-import { ChakraProvider, color, extendTheme } from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
+import { theme } from "./tracknowTheme";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { UserLogin } from "./components/User/UserLogin";
 import { UserSignUp } from "./components/User/UserSignUp";
 import { Welcome } from "./components/Welcome/Welcome";
 import { Home } from "./components/Home/Home";
-import '@fontsource-variable/exo-2'
-// TODO #ff3131 red. change colourtheme
-const theme = extendTheme({
-    styles: {
-        global: {
-            body: {
-                bg: "black",
-                color: "white",
-            },
-        },
-    },
-    colors: {
-        dark: "#1a1919"
-    },
-    fonts: {
-        heading: `'Exo 2 Variable', sans-serif`,
-        body: `'Exo 2 Variable', sans-serif`
-    }
-});
+
 
 export const App = () => (
     <ChakraProvider theme={theme}>
