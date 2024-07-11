473eb96a882a9a8f96a059f0006677e5509526cd
vantage-ola <vantageola4@gmail.com>
2024-07-11 19:26:34 +0100

    feature: proper Create post page

--- tracknow/web/src/components/Navbar/Navbar.tsx
+++ tracknow/web/src/components/Navbar/Navbar.tsx
@@ -7,26 +7,7 @@ import {
     Button,
     Center,
     useColorModeValue,
-    Modal,
-    ModalOverlay,
-    ModalContent,
-    ModalHeader,
-    ModalFooter,
-    ModalBody,
-    ModalCloseButton,
-    useDisclosure,
-    FormControl,
-    FormLabel,
-    Input,
-    Checkbox,
-    Select,
     Link,
-    InputLeftElement,
-    Grid,
-    InputGroup,
-    Textarea,
-    useToast,
-    FormErrorMessage,
     Avatar,
     Menu,
     MenuButton,
@@ -34,12 +15,9 @@ import {
     MenuItem,
     MenuList
 } from "@chakra-ui/react";
-import BeatLoader from "react-spinners/BeatLoader";
-import { PlusSquareIcon, AddIcon } from '@chakra-ui/icons'
+import { AddIcon } from '@chakra-ui/icons'
 import { Link as ReactRouterLink } from 'react-router-dom';
-import { Laptime, SignUpResponse } from "../../Types";
-import { FaCar, FaMapMarkedAlt, FaStopwatch, FaYoutube } from "react-icons/fa";
-import { useLaptimes } from "../../hooks/useLaptimes";
+import { SignUpResponse } from "../../Types";
 
 export const NavbarWelcome = () => (
     <Box px={4} borderBottom={1} borderStyle={'solid'} borderColor={useColorModeValue('dark', 'white')}>
@@ -82,61 +60,6 @@ export const Navbar = () => (
 
 export const NavbarLoggedIn = ({ username }: SignUpResponse) => {
 
-    const { isOpen, onOpen, onClose } = useDisclosure();
-    const { addLaptime } = useLaptimes();
-
-    const [car, setCar] = React.useState("");
-    const [track, setTrack] = React.useState("");
-    const [time, setTime] = React.useState("");
-    const [youtube_link, setYoutubeLink] = React.useState("");
-    const [simracing, setSimracing] = React.useState(false);
-    const [platform, setPlatform] = React.useState("");
-    const [comment, setComment] = React.useState("");
-
-    const [isLoading, setIsLoading] = React.useState(false);
-
-    // add laptimes
-
-    const toast = useToast();
-
-    // handle submitting moment
-    const handleSubmit = async () => {
-
-        const newLaptime: Laptime = {
-            car,
-            track,
-            time,
-            youtube_link,
-            simracing,
-            platform,
-            comment,
-        };
-        setIsLoading(true);
-        try {
-            const response = await addLaptime(newLaptime);
-            toast({
-                title: 'Laptime added successfully',
-                status: "success",
-                duration: 3000,
-                isClosable: true,
-            });
-            onClose();
-            window.location.reload();
-
-        } catch (error) {
-
-            toast({
-                title: "Error while  adding Laptime.",
-                description: (error as Error).message,
-                status: "error",
-                duration: 3000,
-                isClosable: true,
-            });
-        } finally {
-            setIsLoading(false);
-
-        }
-    };
 
     // handle logout
     const handleLogout = () => {
@@ -144,11 +67,7 @@ export const NavbarLoggedIn = ({ username }: SignUpResponse) => {
         localStorage.removeItem("access_token");
         window.location.href = '/login';
         window.location.reload()
-    }
-
-    // regex
-    const youtubeRegex = /http(?:s?):\/\/(?:www\.)?youtu(?:be\.com\/watch\?v=|\.be\/)([\w\-\_]*)(&(amp;)?‌​[\w\?‌​=]*)?/;
-    const timeRegex = /^(\d{1,2})?(\.(\d{2}))?(\.(\d{2}))?(\.(\d{1,3}))?$/;
+    };
 
     return (
         <Box
@@ -175,7 +94,8 @@ export const NavbarLoggedIn = ({ username }: SignUpResponse) => {
                         <Button
                             size={"sm"}
                             variant="navbarButton"
-                            onClick={onOpen}
+                            as={ReactRouterLink}
+                            to={`/user/${username}/create-moments`}
                             leftIcon={<AddIcon />}>
                             Create
                         </Button>
@@ -215,141 +135,6 @@ export const NavbarLoggedIn = ({ username }: SignUpResponse) => {
                 </Flex>
             </Flex>
 
-            {/*add laptime modal*/}
-            <Modal isOpen={isOpen} onClose={onClose} motionPreset="slideInTop" size={'full'}>
-                <ModalOverlay bg="none" backdropFilter="auto" backdropBlur="2px" />
-                <ModalContent>
-                    <ModalHeader color="black">Create A Racing Moment</ModalHeader>
-                    <ModalCloseButton color="black" />
-                    <ModalBody color="black">
-                        <Grid templateColumns="repeat(2, 1fr)" gap={4}>
-
-                            <FormControl isRequired>
-                                <FormLabel>Car</FormLabel>
-                                <InputGroup>
-                                    <InputLeftElement pointerEvents="none">
-                                        <FaCar />
-                                    </InputLeftElement>
-                                    <Input
-                                        focusBorderColor="grey"
-                                        value={car}
-                                        onChange={(e) => setCar(e.target.value)}
-                                        maxLength={50}
-                                    />
-                                </InputGroup>
-                            </FormControl>
-
-                            <FormControl isRequired>
-                                <FormLabel>Track</FormLabel>
-                                <InputGroup>
-                                    <InputLeftElement pointerEvents="none">
-                                        <FaMapMarkedAlt />
-                                    </InputLeftElement>
-                                    <Input
-                                        focusBorderColor="grey"
-                                        value={track}
-                                        onChange={(e) => setTrack(e.target.value)}
-                                        maxLength={50}
-                                    />
-                                </InputGroup>
-                            </FormControl>
-
-                            <FormControl isRequired isInvalid={time && !timeRegex.test(time) ? true : undefined}>
-                                <FormLabel>Laptime</FormLabel>
-                                <InputGroup>
-                                    <InputLeftElement pointerEvents="none">
-                                        <FaStopwatch />
-                                    </InputLeftElement>
-                                    <Input
-                                        focusBorderColor="grey"
-                                        value={time}
-                                        onChange={(e) => setTime(e.target.value)}
-                                        maxLength={10}
-                                    />
-                                </InputGroup>
-                                {time && !timeRegex.test(time) && (
-                                    <FormErrorMessage>Please enter a valid laptime (minutes:seconds.milliseconds)</FormErrorMessage>
-                                )}
-                            </FormControl>
-
-                            <FormControl isRequired isInvalid={youtube_link && !youtubeRegex.test(youtube_link) ? true : undefined}>
-                                <FormLabel>YouTube Link</FormLabel>
-                                <InputGroup>
-                                    <InputLeftElement pointerEvents="none">
-                                        <FaYoutube />
-                                    </InputLeftElement>
-                                    <Input
-                                        focusBorderColor="grey"
-
-                                        value={youtube_link}
-                                        onChange={(e) => setYoutubeLink(e.target.value)}
-                                        maxLength={200}
-
-                                    />
-                                </InputGroup>
-                                {youtube_link && !youtubeRegex.test(youtube_link) && (
-                                    <FormErrorMessage>Please enter a valid YouTube video URL</FormErrorMessage>
-                                )}
-                            </FormControl>
-
-                            <FormControl display="flex" alignItems="center" gridColumn="1 / -1" gap="2" isRequired>
-                                <FormLabel mb="0">Simracing</FormLabel>
-                                <Checkbox
-                                    color="red"
-                                    isChecked={simracing}
-                                    onChange={(e) => setSimracing(e.target.checked)}
-                                />
-                                <InputGroup>
-                                    <Select
-                                        value={platform}
-                                        onChange={(e) => setPlatform(e.target.value)}
-                                        isDisabled={!simracing}
-                                        focusBorderColor="grey"
-
-                                    >
-                                        <option value="">Select platform</option>
-                                        <option value="Assetto Corsa Competizione">Assetto Corsa Competizione</option>
-                                        <option value="iRacing">iRacing</option>
-                                        <option value="Gran Turismo 7">Gran Turismo 7</option>
-                                        <option value="Assetto Corsa">Assetto Corsa </option>
-                                        <option value="BeamNG.drive">BeamNG.drive</option>
-                                        <option value="Formula 1">Formula 1</option>
-                                        <option value="Forza Motorsport">Forza Motorsport</option>
-                                        <option value="Automobilista 2">Automobilista 2</option>
-                                        <option value="Le Mans Ultimate">Le Mans Ultimate</option>
-                                        <option value="Rennsport">Rennsport</option>
-                                        <option value="Dirt Rally 2.0">Dirt Rally 2.0</option>
-                                        <option value="Dirt Rally 2.0">EA SPORTS WRC</option>
-                                        <option value="rFactor 2">rFactor 2</option>
-                                    </Select>
-                                </InputGroup>
-                            </FormControl>
-                            <FormControl gridColumn="1 / -1" isRequired>
-                                <FormLabel>Comment</FormLabel>
-                                <Textarea
-                                    focusBorderColor="grey"
-                                    value={comment}
-                                    onChange={(e) => setComment(e.target.value)}
-                                    maxLength={250}
-                                />
-                            </FormControl>
-
-                        </Grid>
-                    </ModalBody>
-                    <ModalFooter>
-                        <Button color="red.500" variant="ghost" onClick={onClose}>
-                            Close
-                        </Button>
-                        <Button variant='ghost'
-                            onClick={handleSubmit}
-                            isDisabled={!car || !track || !time || !timeRegex.test(time) || !youtube_link || !youtubeRegex.test(youtube_link) || !simracing || !platform || !comment}
-                            cursor={car && track && time && youtube_link && simracing && platform && comment ? "pointer" : "not-allowed"}
-                            isLoading={isLoading}
-                            spinner={<BeatLoader size={8} color='red' />}
-                        >Submit</Button>
-                    </ModalFooter>
-                </ModalContent>
-            </Modal>
         </Box>
     );
 };
