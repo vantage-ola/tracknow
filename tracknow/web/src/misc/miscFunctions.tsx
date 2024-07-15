ba09e00a2d2dbb2df973b35b99a88f4bdf68f331
vantage-ola <vantageola4@gmail.com>
2024-07-15 11:15:09 +0100

    fix: lazy load.

--- tracknow/web/src/misc/miscFunctions.tsx
+++ tracknow/web/src/misc/miscFunctions.tsx
@@ -1,6 +1,8 @@
 import { AspectRatio, Box } from "@chakra-ui/react";
 import * as React from "react";
 import { useNavigate } from "react-router-dom";
+import useIntersectionObserver from '@react-hook/intersection-observer';
+import Youtube from 'react-lazyload-youtube';
 
 // get logged in username function
 const useMiscFunctions = () => {
@@ -21,55 +23,35 @@ const useMiscFunctions = () => {
         navigate('/home');
 
     };
+
     // react memo, only reloads the youtube if the props change  React.memo(), didnt work lol
     // some bugs with post read more, it reloads the yt clip, dont know why
     const LazyLoadYoutubeEmbed = ({ youtubeLink }: { youtubeLink: string }) => {
-        const [isVisible, setIsVisible] = React.useState(false);
-        const ref = React.useRef<HTMLDivElement>(null);
-
-        React.useEffect(() => {
-            const observer = new IntersectionObserver(
-                ([entry]) => {
-                    if (entry.isIntersecting) {
-                        setIsVisible(true);
-                        observer.unobserve(entry.target);
-                    }
-                },
-                {
-                    rootMargin: "0px",
-                    threshold: 0.1,
-                }
-            );
-
-            if (ref.current) {
-                observer.observe(ref.current);
-            }
-
-            return () => {
-                if (ref.current) {
-                    observer.unobserve(ref.current);
-                }
-            };
-        }, []);
 
         const roundframe = {
             borderRadius: "15px",
             overflow: "hidden",
         };
-
+        const youtubeID = youtubeLink.split("v=")[1]
         return (
-            <Box mt={1} ref={ref} >
-                {isVisible && (
-                    <AspectRatio ratio={16 / 9}>
-                        <iframe
-                            style={roundframe}
-                            src={`https://www.youtube.com/embed/${youtubeLink.split("v=")[1]}`
-                            }
-                            title="YouTube video player"
-                            allowFullScreen
-                        />
-                    </AspectRatio>
-                )}
+            <Box mt={1}  >
+
+                <AspectRatio ratio={16 / 9}>
+                    <iframe
+                        style={roundframe}
+                        src={`https://www.youtube.com/embed/${youtubeID}`
+                        }
+                        srcDoc={`<style>
+                            *{padding:0;margin:0;overflow:hidden}html,body{height:100%}img,
+                            span{position:absolute;width:100%;top:0;bottom:0;margin:auto}
+                            span{height:1.5em;text-align:center;font:48px/1.5 sans-serif;color:white;text-shadow:0 0 0.5em black}
+                            </style>
+                            <a href=https://www.youtube.com/embed/${youtubeID}?autoplay=1>
+                            <img src=https://img.youtube.com/vi/${youtubeID}/hqdefault.jpg><span>▶</span></a>"`}
+                        allowFullScreen
+                    />
+                </AspectRatio>
+
             </Box>
         );
     };
