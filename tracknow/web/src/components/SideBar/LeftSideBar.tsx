fc8343d5ddda7ee53cc14230b699639553ffc01c
vantage-ola <vantageola4@gmail.com>
2024-07-21 18:46:38 +0100

    added more items to the left side bar

--- tracknow/web/src/components/SideBar/LeftSideBar.tsx
+++ tracknow/web/src/components/SideBar/LeftSideBar.tsx
@@ -1,7 +1,9 @@
-import { Box, Center, Divider, Flex, FlexProps, Icon, Link, Stack } from '@chakra-ui/react';
-import { FiHome, FiCompass } from 'react-icons/fi'
-import { IconType } from 'react-icons'
-import { Link as ReactRouterLink, useNavigate } from 'react-router-dom';
+import { Box, Center, Divider, Flex, FlexProps, Icon, Link, Stack, Text } from '@chakra-ui/react';
+import { FiHome, FiAward, FiGlobe, FiInfo, FiSettings, FiDollarSign, FiCalendar } from 'react-icons/fi';
+import { IconType } from 'react-icons';
+import { Link as ReactRouterLink } from 'react-router-dom';
+import { BiGhost } from "react-icons/bi";
+import { FaGithub } from "react-icons/fa";
 
 interface LinkItemProps {
     name: string
@@ -14,23 +16,29 @@ interface SideItemProps extends FlexProps {
 
 const LinkItems: Array<LinkItemProps> = [
     { name: 'Home', icon: FiHome },
-    { name: 'Explore', icon: FiCompass },
-]
+    { name: 'Discover', icon: FiGlobe },
+    { name: 'Events', icon: FiCalendar },
+    { name: 'Leaderboard', icon: FiAward },
+    { name: 'Setups', icon: FiSettings },
+    { name: 'Ghosts', icon: BiGhost },
+    { name: 'About tracknow', icon: FiInfo },
+    { name: 'Contribute', icon: FaGithub },
+    { name: 'Donate', icon: FiDollarSign },
+];
+
+const linkMap: { [key: string]: string } = {
+    Home: "/home",
+    // Add more links here
+};
+
 
 const SideBarItem = ({ icon, children, ...rest }: SideItemProps) => {
+    const link = linkMap[String(children)] || "/";
 
-    let link = "/";
-    if (children === "Home") {
-        link = "/home";
-    }
-    if (children === "Explore") {
-        link = "/explore";
-    }
-
-    return (
+    return linkMap[String(children)] ? (
         <Link as={ReactRouterLink} to={link} style={{ textDecoration: 'none' }}>
             <Flex align="center"
-                p="2"
+                p="4"
                 mx="4"
                 borderRadius="lg"
                 role="group"
@@ -52,7 +60,32 @@ const SideBarItem = ({ icon, children, ...rest }: SideItemProps) => {
                 {children}
             </Flex>
         </Link>
-    )
+    ) : (
+        <div>
+            <Flex align="center"
+                p="4"
+                mx="4"
+                borderRadius="lg"
+                role="group"
+                cursor="not-allowed"
+                _hover={{
+                    bg: 'lightdark',
+                    color: 'white',
+                }}>
+                {icon && (
+                    <Icon
+                        mr="4"
+                        fontSize="16"
+                        _groupHover={{
+                            color: 'white',
+                        }}
+                        as={icon}
+                    />
+                )}
+                {children}
+            </Flex>
+        </div>
+    );
 };
 
 const LeftSideBar = () => {
@@ -67,7 +100,14 @@ const LeftSideBar = () => {
                         </SideBarItem>
                     ))}
                 </Stack>
+                <Center>
+                    <Box mt={2} fontSize="10px" color="GrayText">
+                        tracknow, © 2024, All rights reserved
+                    </Box>
+                </Center >
             </Box>
+
+
 
         </>
     )
