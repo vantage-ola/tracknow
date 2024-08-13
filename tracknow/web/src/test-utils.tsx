88577496b09f8a9cf60206358ea4e435b2ac876b
eric-kingu <freedom.geek7@gmail.com>
2024-08-12 21:10:53 -0300

    Fix Time Format

--- tracknow/web/src/test-utils.tsx
+++ tracknow/web/src/test-utils.tsx
@@ -5,7 +5,13 @@ import { ChakraProvider, theme } from "@chakra-ui/react"
 const AllProviders = ({ children }: { children?: React.ReactNode }) => (
   <ChakraProvider theme={theme}>{children}</ChakraProvider>
 )
-
+/*
+describe('Time format', () => {
+  test('Time format is 00:00:00.000', () => {
+    expect(UserAddLaptimes("00:00:00.000")).toBe("00:00:00.000")
+  })
+})
+*/
 const customRender = (ui: React.ReactElement, options?: RenderOptions) =>
   render(ui, { wrapper: AllProviders, ...options })
 
