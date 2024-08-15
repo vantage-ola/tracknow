a5168b16a10970996525837a37d9b8dd43cbd4eb
vantage-ola <vantageola4@gmail.com>
2024-08-15 15:46:14 +0100

    refactor: create Main Layout to center components inside

---
+++ tracknow/web/src/components/MainLayout.tsx
@@ -0,0 +1,57 @@
+import * as React from "react";
+import { Flex, Box, useBreakpointValue, useDisclosure, } from '@chakra-ui/react';
+import LeftSideBar from './SideBar/LeftSideBar';
+import RightSideBar from './SideBar/RightSideBar';
+import MobileDrawer from "../misc/MobileDrawer";
+import { Outlet } from 'react-router-dom';
+
+type MainLayoutProps = {
+    isOpen: boolean;
+    onOpen: () => void;
+    onClose: () => void;
+};
+
+const MainLayout: React.FC<MainLayoutProps> = ({ isOpen, onOpen, onClose }) => {
+
+
+    const isMobile = useBreakpointValue({ base: true, md: false });
+
+    return (
+        <Flex mt={10} bg="dark" height="calc(100vh - 45px)">
+            {/* Left section */}
+
+            {isMobile ? (
+                <MobileDrawer isOpen={isOpen} onClose={onClose}>
+                    <LeftSideBar />
+                </MobileDrawer>
+            ) : (
+                <Box
+                    flex="1"
+                    borderRight="1px solid #323536"
+                    overflowY="auto"
+                    height="full"
+                >
+                    <LeftSideBar />
+                </Box>
+            )}
+
+            {/* Middle section */}
+            <Box flex="3" rounded={'sm'} my={1} mx={[0, 5]} overflow={'hidden'} borderRadius={"1px"} overflowY="auto" height="full">
+                <Outlet />
+            </Box>
+
+            {/* Right section */}
+            <Box
+                flex="1"
+                overflowY="auto"
+                display={["none", "none", "block"]}
+                height="full"
+            >
+                <RightSideBar /> {/*  right sidebar content*/}
+            </Box>
+
+        </Flex>
+    );
+};
+
+export default MainLayout;
